695374e97288003749e762669c739d44331c9565
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-26 03:42:50 +0300

    Update MyAvatar.js

--- src/components/MyAvatar.js
+++ src/components/MyAvatar.js
@@ -1,23 +1,23 @@
-// hooks
-import useAuth from '../hooks/useAuth';
-// utils
-import createAvatar from '../utils/createAvatar';
-//
-import Avatar from './Avatar';
+  // hooks
+  import useAuth from '../hooks/useAuth';
+  // utils
+  import createAvatar from '../utils/createAvatar';
+  //
+  import Avatar from './Avatar';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-export default function MyAvatar({ ...other }) {
-  const { user } = useAuth();
+  export default function MyAvatar({ ...other }) {
+    const { user } = useAuth();
 
-  return (
-    <Avatar
-      src={user?.photoURL}
-      alt={user?.displayName}
-      color={user?.photoURL ? 'default' : createAvatar(user?.name).color}
-      {...other}
-    >
-      {createAvatar(user?.name).name}
-    </Avatar>
-  );
-}
+    return (
+      <Avatar
+        src={user?.photoURL}
+        alt={user?.displayName}
+        color={user?.photoURL ? 'default' : createAvatar(user?.name).color}
+        {...other}
+      >
+        {createAvatar(user?.name).name}
+      </Avatar>
+    );
+  }
