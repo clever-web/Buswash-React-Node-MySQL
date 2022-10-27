4437836bd58dc6b77346edc490a89d78bbcaa2e8
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-27 07:57:28 +0300

    Update useCustomer.js

--- src/hooks/useCustomer.js
+++ src/hooks/useCustomer.js
@@ -1,27 +1,27 @@
-/* eslint-disable prettier/prettier */
-import { useDispatch } from 'react-redux';
+  /* eslint-disable prettier/prettier */
+  import { useDispatch } from 'react-redux';
 
-// redux
-import { addCustomer, editCustomer, deleteCustomer } from '../redux/slices/user';
+  // redux
+  import { addCustomer, editCustomer, deleteCustomer } from '../redux/slices/user';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-export default function useCustomer() {
-  const dispatch = useDispatch();
+  export default function useCustomer() {
+    const dispatch = useDispatch();
 
-  return {
-    // --------------  Editing part ---------------------
-    editCustomer: ({ data }) => {
-      dispatch(editCustomer({ data }));
-    },
-    // --------------  Creating part ---------------------
-    addCustomer: ({ data }) => {
-      dispatch(addCustomer({ data }));
-    },
+    return {
+      // --------------  Editing part ---------------------
+      editCustomer: ({ data }) => {
+        dispatch(editCustomer({ data }));
+      },
+      // --------------  Creating part ---------------------
+      addCustomer: ({ data }) => {
+        dispatch(addCustomer({ data }));
+      },
 
-    // --------------  Delete Employee ---------------------
-    deleteCustomer: (id) => {
-      dispatch(deleteCustomer(id));
-    },
-  };
-}
+      // --------------  Delete Employee ---------------------
+      deleteCustomer: (id) => {
+        dispatch(deleteCustomer(id));
+      },
+    };
+  }
