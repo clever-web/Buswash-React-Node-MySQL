3ec59e532de2147cb358ed3cb08cdfc14245783e
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-27 07:57:22 +0300

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,18 +1,18 @@
-import { useContext } from 'react';
-//
-import { AuthContext } from '../contexts/JWTContext';
-// import { AuthContext } from '../contexts/Auth0Context';
-// import { AuthContext } from '../contexts/FirebaseContext';
-// import { AuthContext } from '../contexts/AwsCognitoContext';
+  import { useContext } from 'react';
+  //
+  import { AuthContext } from '../contexts/JWTContext';
+  // import { AuthContext } from '../contexts/Auth0Context';
+  // import { AuthContext } from '../contexts/FirebaseContext';
+  // import { AuthContext } from '../contexts/AwsCognitoContext';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-const useAuth = () => {
-  const context = useContext(AuthContext);
+  const useAuth = () => {
+    const context = useContext(AuthContext);
 
-  if (!context) throw new Error('Auth context must be use inside AuthProvider');
+    if (!context) throw new Error('Auth context must be use inside AuthProvider');
 
-  return context;
-};
+    return context;
+  };
 
-export default useAuth;
+  export default useAuth;
