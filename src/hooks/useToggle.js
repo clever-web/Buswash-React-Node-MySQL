4afb6bc6c28e67649aaf47f8f699c3842fcd5535
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-28 22:36:10 +0300

    Update useToggle.js

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+  import { useState } from 'react';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-export default function useToggle(defaultChecked) {
-  const [toggle, setToggle] = useState(defaultChecked || false);
+  export default function useToggle(defaultChecked) {
+    const [toggle, setToggle] = useState(defaultChecked || false);
 
-  return {
-    toggle,
-    onToggle: () => setToggle(!toggle),
-    onOpen: () => setToggle(true),
-    onClose: () => setToggle(false),
-    setToggle,
-  };
-}
+    return {
+      toggle,
+      onToggle: () => setToggle(!toggle),
+      onOpen: () => setToggle(true),
+      onClose: () => setToggle(false),
+      setToggle,
+    };
+  }
