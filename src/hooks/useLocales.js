ad704fcd3cb4413e5f630e8d91b94409b6614bbd
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-28 22:35:56 +0300

    Update useLocales.js

--- src/hooks/useLocales.js
+++ src/hooks/useLocales.js
@@ -1,28 +1,28 @@
-import { useTranslation } from 'react-i18next';
-import useSettings from './useSettings';
-// config
-import { allLangs, defaultLang } from '../config';
+  import { useTranslation } from 'react-i18next';
+  import useSettings from './useSettings';
+  // config
+  import { allLangs, defaultLang } from '../config';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-export default function useLocales() {
-  const { i18n, t: translate } = useTranslation();
+  export default function useLocales() {
+    const { i18n, t: translate } = useTranslation();
 
-  const { onChangeDirectionByLang } = useSettings();
+    const { onChangeDirectionByLang } = useSettings();
 
-  const langStorage = localStorage.getItem('i18nextLng');
+    const langStorage = localStorage.getItem('i18nextLng');
 
-  const currentLang = allLangs.find((_lang) => _lang.value === langStorage) || defaultLang;
+    const currentLang = allLangs.find((_lang) => _lang.value === langStorage) || defaultLang;
 
-  const handleChangeLanguage = (newlang) => {
-    i18n.changeLanguage(newlang);
-    onChangeDirectionByLang(newlang);
-  };
+    const handleChangeLanguage = (newlang) => {
+      i18n.changeLanguage(newlang);
+      onChangeDirectionByLang(newlang);
+    };
 
-  return {
-    onChangeLang: handleChangeLanguage,
-    translate: (text, options) => translate(text, options),
-    currentLang,
-    allLangs,
-  };
-}
+    return {
+      onChangeLang: handleChangeLanguage,
+      translate: (text, options) => translate(text, options),
+      currentLang,
+      allLangs,
+    };
+  }
