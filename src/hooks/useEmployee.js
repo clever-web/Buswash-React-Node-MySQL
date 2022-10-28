aef8290f60ee1535af7594d00a77b2b4fe2672d9
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-28 22:35:52 +0300

    Update useEmployee.js

--- src/hooks/useEmployee.js
+++ src/hooks/useEmployee.js
@@ -1,27 +1,27 @@
-/* eslint-disable prettier/prettier */
-import { useDispatch } from 'react-redux';
+  /* eslint-disable prettier/prettier */
+  import { useDispatch } from 'react-redux';
 
-// redux
-import { addEmployee, editEmployee, deleteEmployee } from '../redux/slices/user';
+  // redux
+  import { addEmployee, editEmployee, deleteEmployee } from '../redux/slices/user';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-export default function useEmployee() {
-  const dispatch = useDispatch();
+  export default function useEmployee() {
+    const dispatch = useDispatch();
 
-  return {
-    // --------------  Editing part ---------------------
-    editEmployee: ({ data }) => {
-      dispatch(editEmployee({ data }));
-    },
-    // --------------  Creating part ---------------------
-    addEmployee: ({ data }) => {
-      dispatch(addEmployee({ data }));
-    },
+    return {
+      // --------------  Editing part ---------------------
+      editEmployee: ({ data }) => {
+        dispatch(editEmployee({ data }));
+      },
+      // --------------  Creating part ---------------------
+      addEmployee: ({ data }) => {
+        dispatch(addEmployee({ data }));
+      },
 
-    // --------------  Delete Employee ---------------------
-    deleteEmployee: (id) => {
-      dispatch(deleteEmployee(id));
-    },
-  };
-}
+      // --------------  Delete Employee ---------------------
+      deleteEmployee: (id) => {
+        dispatch(deleteEmployee(id));
+      },
+    };
+  }
