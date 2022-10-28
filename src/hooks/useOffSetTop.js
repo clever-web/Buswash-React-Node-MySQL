41f09d721d9ab55696024a6682af1eb6deff3f48
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-28 22:35:59 +0300

    Update useOffSetTop.js

--- src/hooks/useOffSetTop.js
+++ src/hooks/useOffSetTop.js
@@ -1,26 +1,26 @@
-import { useState, useEffect } from 'react';
+  import { useState, useEffect } from 'react';
 
-// ----------------------------------------------------------------------
+  // ----------------------------------------------------------------------
 
-export default function useOffSetTop(top) {
-  const [offsetTop, setOffSetTop] = useState(false);
-  const isTop = top || 100;
+  export default function useOffSetTop(top) {
+    const [offsetTop, setOffSetTop] = useState(false);
+    const isTop = top || 100;
 
-  useEffect(() => {
-    window.onscroll = () => {
-      if (window.pageYOffset > isTop) {
-        setOffSetTop(true);
-      } else {
-        setOffSetTop(false);
-      }
-    };
-    return () => {
-      window.onscroll = null;
-    };
-  }, [isTop]);
+    useEffect(() => {
+      window.onscroll = () => {
+        if (window.pageYOffset > isTop) {
+          setOffSetTop(true);
+        } else {
+          setOffSetTop(false);
+        }
+      };
+      return () => {
+        window.onscroll = null;
+      };
+    }, [isTop]);
 
-  return offsetTop;
-}
+    return offsetTop;
+  }
 
-// Usage
-// const offset = useOffSetTop(100);
+  // Usage
+  // const offset = useOffSetTop(100);
