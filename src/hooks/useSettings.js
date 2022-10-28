fe0b2e5e583019a727e99a33eb8c9b7c8527a85e
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-28 22:36:04 +0300

    Update useSettings.js

--- src/hooks/useSettings.js
+++ src/hooks/useSettings.js
@@ -1,8 +1,8 @@
-import { useContext } from 'react';
-import { SettingsContext } from '../contexts/SettingsContext';
+    import { useContext } from 'react';
+    import { SettingsContext } from '../contexts/SettingsContext';
 
-// ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
 
-const useSettings = () => useContext(SettingsContext);
+    const useSettings = () => useContext(SettingsContext);
 
-export default useSettings;
+    export default useSettings;
